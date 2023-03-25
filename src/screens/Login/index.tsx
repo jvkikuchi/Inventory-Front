0c856275dc26f8114e82d8c5e07d260e1995a82c
jvkikuchi <joao.kikuchi@bgcbrasil.com.br>
2023-03-25 15:43:14 -0300

    feat: placeholder inputs effects using labels

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -14,7 +14,20 @@ import {
 import {Icon} from '../../components/Icon';
 
 export const Login = () => {
-  const [showPassword, setShowPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [isEmailFocused, setIsEmailFocused] = useState<boolean>(false);
+  const [isPasswordFocused, setPasswordFocused] = useState<boolean>(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  };
+
   return (
     <Box alignItems="center" minW={100} mt={48}>
       <Heading fontSize="5xl" fontWeight="medium">
@@ -22,20 +35,49 @@ export const Login = () => {
       </Heading>
       <FormControl mt={16}>
         <VStack minW="100%" padding={'40px'}>
-          <FormControl.Label>Email</FormControl.Label>
+          <FormControl.Label
+            position="absolute"
+            left={10}
+            top={isEmailFocused || email !== '' ? '24px' : '56px'}
+            _text={{
+              color: isEmailFocused ? 'orange.500' : 'gray.500',
+            }}>
+            EMAIL
+          </FormControl.Label>
           <Input
+            onChangeText={newText => setEmail(newText)}
+            onFocus={() => setIsEmailFocused(true)}
+            onBlur={() => {
+              if (email === '') {
+                setIsEmailFocused(false);
+              }
+            }}
             borderBottomWidth={2}
             variant="underlined"
             type="text"
-            _focus={{
-              borderBottomColor: 'orange.500',
-              borderColor: 'orange.500',
-            }}
+            borderBottomColor={isEmailFocused ? 'orange.500' : 'gray.500'}
+            mb={10}
           />
-          <FormControl.Label mt={7}>Password</FormControl.Label>
+          <FormControl.Label
+            mt={7}
+            position="absolute"
+            left={10}
+            top={isPasswordFocused || password !== '' ? '88px' : '118px'}
+            _text={{
+              color: isPasswordFocused ? 'orange.500' : 'gray.500',
+            }}>
+            SENHA
+          </FormControl.Label>
           <Input
+            onChangeText={newText => setPassword(newText)}
+            onFocus={() => setPasswordFocused(true)}
+            onBlur={() => {
+              if (password === '') {
+                setPasswordFocused(false);
+              }
+            }}
             borderBottomWidth={2}
-            _focus={{borderBottomColor: 'orange.500'}}
+            borderBottomColor={isPasswordFocused ? 'orange.500' : 'gray.500'}
             variant="underlined"
             type={showPassword ? 'text' : 'password'}
             InputRightElement={
@@ -56,7 +98,10 @@ export const Login = () => {
               bgColor="orange.500"
               minW="200px"
               borderRadius={30}
-              padding="15px">
+              padding="15px"
+              onPress={handleSubmit}
+              isLoading={isLoading}
+              _spinner={{size: 'lg'}}>
               <Text fontSize={24} color="white" fontWeight="medium">
                 ENTRAR
               </Text>
